32f1bc25ea3c7ebdfcc6ee6d243a04ffe7356792
prakash <56664619+loveleypra@users.noreply.github.com>
2023-01-20 16:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function myfunction()
 {
     var un= document.forms["form"]["uname"].value;
     var pw= document.forms["form"]["psw"].value;
-    if (un=="1234" &&  pw=="1234")
+    if (pw=="1234")
     {
         window.location.href="home.html";
 
     }
     else
     {
-        alert("invalid  username and password ");
+        alert("incorrect username and password ");
     }
 }
